c502ad3a75127ce10593bce1804dce4ab005ef9d
Maycol Michel Mohr <maycolmohr@yahoo.com.br>
2021-09-21 17:07:04 +0200

    add new category - test

--- src/main/java/com/devsuperior/ProjetoJavawebnoSpringBoot/ProjetoJavaWebNoSpringBootApplication.java
+++ src/main/java/com/devsuperior/ProjetoJavawebnoSpringBoot/ProjetoJavaWebNoSpringBootApplication.java
@@ -29,22 +29,28 @@ public class ProjetoJavaWebNoSpringBootApplication implements CommandLineRunner{
 	public void run(String... args) throws Exception {
 		Category cat1 = new Category(null, "Electronics");
 		Category cat2 = new Category(null, "Books");
+		Category cat3 = new Category(null, "Sports");
 
 		Product p1 = new Product(null, "TV", 2200.00, cat1);
 		Product p2 = new Product(null, "Domain Driven Design", 120.00, cat2);
 		Product p3 = new Product(null, "PS5", 2800.00, cat1);
 		Product p4 = new Product(null, "Docker", 100.00, cat2);
+		Product p5 = new Product(null, "Soccer Ball", 230.00, cat3);
 
 		cat1.getProducts().addAll(Arrays.asList(p1, p3));
 		cat2.getProducts().addAll(Arrays.asList(p2, p4));
+		cat3.getProducts().addAll(Arrays.asList(p5));
 
 		
 		categoryRepository.save(cat1);
 		categoryRepository.save(cat2);
+		categoryRepository.save(cat3);
 		
 		productRepository.save(p1);
 		productRepository.save(p2);
 		productRepository.save(p3);
 		productRepository.save(p4);
+		productRepository.save(p5);
+		
 	}
 }
